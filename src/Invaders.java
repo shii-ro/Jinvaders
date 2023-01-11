99ef46958bad80b9aff74e280800c6119b6a2b1e
shii-ro <wesley.7.castro@gmail.com>
2023-01-11 15:02:56 -0300

    Added frame limiter, should run at 60hz now

--- src/Invaders.java
+++ src/Invaders.java
@@ -4,6 +4,9 @@ import java.nio.file.Files;
 import java.nio.file.Path;
 import java.nio.file.Paths;
 import java.awt.event.KeyEvent;
+import java.util.Timer;
+import java.util.TimerTask;
+
 import static java.lang.Thread.*;
 
 
@@ -31,22 +34,28 @@ public class Invaders {
         display.init(key);
         display.repaint();
 
-        while(!cpu.error){
-            int total_cycles = 0;
-            while(total_cycles < CYCLES_PER_MIDFRAME && !cpu.error) {
-                cpu.cycle();
-                total_cycles += cpu.cycles;
-            }
-            // mid frame interrupt
-            cpu.midFrameInterruptHandler();
-            while(total_cycles < CYCLES_PER_MIDFRAME && !cpu.error) {
-                cpu.cycle();
-                total_cycles += cpu.cycles;
-            }
-            cpu.vblankInterruptHandler();
-            sleep(10);
-            display.repaint();
-        }
+        TimerTask run = new TimerTask() {
+            @Override
+            public void run() {
+                int total_cycles = 0;
 
+                while (total_cycles < CYCLES_PER_MIDFRAME && !cpu.error) {
+                    cpu.cycle();
+                    total_cycles += cpu.cycles;
+                }
+                // mid frame interrupt
+                cpu.midFrameInterruptHandler();
+                while (total_cycles < CYCLES_PER_MIDFRAME && !cpu.error) {
+                    cpu.cycle();
+                    total_cycles += cpu.cycles;
+                }
+                cpu.vblankInterruptHandler();
+                display.repaint();
+            }
+        };
+
+        int ms = 1000 / 60;
+        Timer timer = new Timer();
+        timer.scheduleAtFixedRate(run, 0, ms);
     }
 }
